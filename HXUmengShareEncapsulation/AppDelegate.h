0b35602f36f719a64e976f2d96cc386ee6c3b3f0
derekhuangxu <huangwade3@163.com>
2016-05-22 01:29:19 +0800

    first

---
+++ HXUmengShareEncapsulation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXUmengShareEncapsulation
+//
+//  Created by Derek on 16/5/22.
+//  Copyright © 2016年 huangxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXUmengShareEncapsulation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXUmengShareEncapsulation
+//
+//  Created by Derek on 16/5/22.
+//  Copyright © 2016年 huangxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
